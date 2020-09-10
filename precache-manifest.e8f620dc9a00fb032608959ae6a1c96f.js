b96cec6406051a550f6237f4da670442107fccc0
Atharva Avhad <gatly0145@gmail.com>
2020-09-10 15:25:01 +0530

    Updates

---
+++ precache-manifest.e8f620dc9a00fb032608959ae6a1c96f.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4c6e8db0d6aeaa2152bb373d1d18be29",
+    "url": "/shankaravhadgroup/index.html"
+  },
+  {
+    "revision": "ce294bec376d37d235de",
+    "url": "/shankaravhadgroup/static/css/2.ea6b7726.chunk.css"
+  },
+  {
+    "revision": "8f9c3d2092dab1629594",
+    "url": "/shankaravhadgroup/static/css/main.84aa1627.chunk.css"
+  },
+  {
+    "revision": "ce294bec376d37d235de",
+    "url": "/shankaravhadgroup/static/js/2.b37adc4e.chunk.js"
+  },
+  {
+    "revision": "361388382a7cba0b7cd58ae9b81faa0e",
+    "url": "/shankaravhadgroup/static/js/2.b37adc4e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8f9c3d2092dab1629594",
+    "url": "/shankaravhadgroup/static/js/main.0c3d29ff.chunk.js"
+  },
+  {
+    "revision": "0ebb39aa9a409764cf0c",
+    "url": "/shankaravhadgroup/static/js/runtime-main.2c498db2.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/shankaravhadgroup/static/media/logo.5d5d9eef.svg"
+  }
+]);
